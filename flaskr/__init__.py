4c619dc7c05c4274d32050a5bcd0e8d9b776f251
EFrankhouse <efrankho@kent.edu>
2019-03-04 12:49:08 -0500

    Added routes to each specific page

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,5 +3,34 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return render_template("hello.html")
+def renderLanding():
+    return render_template("landing.html")
+
+@app.route("/login")
+def renderLogin():
+    return render_template("login/login.html")
+
+@app.route("/login/forgot")
+def renderForgotPassword():
+    return render_template("login/forgot.html")
+
+@app.route("/register")
+def renderRegister():
+    return render_template("register/register.html")
+
+@app.route("/profile")
+def renderProfile():
+    return render_template("profile/profile.html")
+
+@app.route("/profile/myevents")
+def renderMyEvents():
+    return render_template("profile/myevents.html")
+
+@app.route("/events")
+def renderEvents():
+    return render_template("events/events.html")  
+
+@app.route("/settings")
+def renderSettings():
+    return render_template("settings/settings.html")
+
